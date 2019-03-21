01df82dec10e4fd079fb9579b72f62ef4566139a
sanderoost <sander-oostenbrink@hotmail.com>
2019-03-21 23:38:45 +0100

    Added working api request and database

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const axios = require('axios');
 var slug = require('slug');
+var mongo = require('mongodb');
 var bodyParser = require('body-parser');
 
-var api = "http://www.omdbapi.com/?i=tt3896198&apikey=";
-var apikey = "e1225bf"
+const app = express();
+const port = 3000;
+
+require('dotenv').config()
+
+var db = null
+var url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT
+console.log(url);
+mongo.MongoClient.connect(url, function (err, client) {
+  if (err) throw err
+  db = client.db(process.env.DB_NAME)
+})
+
 app
     .use('/static', express.static('static'))
     .use(bodyParser.urlencoded({extended: true}))
@@ -19,39 +30,22 @@ app
 var data = [
     {
       id: 'how-i-met-your-mother',
-      title: 'How i met your mother'
+      img: 'static/how-i-met-your-mother.jpg',
+      title: 'How i met your mother',
+      rating: '9'
     },
     {
       id: 'lost',
-      title: 'Lost'
+      img: 'static/lost.jpg',
+      title: 'Lost',
+      rating: '8,4'
     }
   ]
 
+// Pathing url
 function homepage(req, res){
   res.render("homepage.pug", {data: data});
 }
-
-function push(req, res){
-  var id = slug(req.body.film).toLowerCase()
-
-  data.push({
-    id: id,
-    title: req.body.film,
-  })
-
-  res.redirect('/')
-}
-
-function remove(req, res) {
-  var id = req.params.id
-
-  data = data.filter(function (value) {
-    return value.id !== id
-  })
-
-  res.json({status: 'ok'})
-}
-
 function about(req, res){
   res.render("about.pug");
 }
@@ -59,4 +53,35 @@ function notfound(req, res){
   res.status(404).render("notfound.pug");
 }
 
-app.listen(port, () => console.log(`Server is gestart op port: ${port}!`))
+
+// Push data into array
+function push(req, res){
+  var id = slug(req.body.film).toLowerCase()
+  const api = "http://www.omdbapi.com/?t=" + id + "&apikey=" + process.env.APIKEY;
+  //Make a reqeust to the omdbapi
+  axios.get(api)
+   .then(response=> { 
+        data.push({
+        id: id,
+        img: response.data.Poster,
+        title: req.body.film,
+        rating: response.data.imdbRating
+      })
+      //after the data has been added redirect to the home page 
+    res.redirect('/')
+    })
+  .catch(data=>console.log(error.response.data))
+}
+
+
+function remove(req, res) {
+  var id = req.params.id
+  data = data.filter(function (value) {
+    return value.id !== id
+  })
+
+  res.json({status: 'ok'})
+}
+
+
+app.listen(port)
--- static/index.js
+++ static/index.js
@@ -1,35 +1,45 @@
-var remove = document.getElementById('js-remove');
+//jqeury for the sortable function
+$( function() {
+  $( "#sortable" ).sortable();
+  $( "#sortable" ).disableSelection();
+} );
+
+
 var enable = document.getElementById('enable');
 var form = document.getElementsByTagName("form")[0];
 
-if (remove) {
-  remove.addEventListener('click', onremove)
-}
+// creates button function for every id
+const buttons = document.querySelectorAll('ul button')
+buttons.forEach(function(currentBtn){
+  currentBtn.addEventListener('click', onremove)
+})
+
 if (enable) {
   enable.addEventListener('click', enableform)
 }
+
+// Hide add button if the screen has 5 li
+if (document.getElementsByTagName("section li").length > 4) {
+  enable.classList.add("hide");
+}
+
 function enableform(){
 	form.classList="show";
   enable.classList.add("hide");
 }
 
+
 function onremove(ev) {
   var node = ev.target
   var id = node.dataset.id
-
-
   var res = new XMLHttpRequest()
-
   res.open('DELETE', '/' + id)
   res.onload = onload
   res.send()
-
   function onload() {
     if (res.status !== 200) {
       throw new Error('Could not delete!')
     }
-
-
     window.location = '/'
   }
 }
